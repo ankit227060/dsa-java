49bcc6a721a0a4c67e3e9a81b1d4c5d8a9ef24e1
Ankit <ankit_227060@saitm.org>
2026-02-07 02:23:29 +0530

    palindrome

---
+++ Foundation/problems/9palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev =0;
+        int rem =0;
+        int n = x;
+        while(n>0){
+            rem = n%10;
+            rev = (rev*10)+rem;
+            n=n/10;
+        }
+        if(rev==x){
+            return true;
+        }else return false ;
+    }
+}
+
+//time complexity: O(log n)
+//space complexity: O(1)
